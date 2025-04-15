1e49707133d9c2c0a5024161365747dbf8bed4de
Clarajos <clara.seibel@students.mq.edu.au>
2025-04-15 17:47:38 +1000

    Meeting with John

--- r/WorkingAnalysis.R
+++ r/WorkingAnalysis.R
@@ -1,9 +1,12 @@
 library(readr)
 library(vegan)
 #import Data 
-Fish <- read_csv("Data/eDNA_Data.csv")
-Chem18 <- read_csv("Data/Waterchem2018.csv")
-Nutrients18 <- read_csv("Data/Nutrients2018.csv")
+Fish <- read.csv("Data/eDNA_Data.csv")
+View(Fish)
+Chem18 <- read.csv("Data/Waterchem2018.csv", header=TRUE)
+View(Chem18)
+head(Chem18)
+Nutrients18 <- read.csv("Data/Nutrients2018.csv")
 Pesticides18 <- read_csv("Data/Pesticides2018.csv")
 Sediments18 <- read_csv("Data/Sediment2018.csv")
 Chem20 <- read_csv("Data/Waterchem2020.csv")
@@ -38,6 +41,9 @@ cols[6,]=c(45,54)
 cols[7,]=c(55,64)
 cols[8,]=c(65,68)
 #keep filling in- see if g value repeat later. Line 14 gsindex sould be larger than below as its for whole world rather than certain places
+#breaks it up into sites
+
+
 
 
 #species estimate for eac estuary
@@ -45,6 +51,7 @@ g=array() # blank array
 bootstrapgsi=array()
 r=array()
 h=array() #shannons
+fa=array()
 
 for(i in 1:8) { #looking through first 2 rows, opening for loop
   n=rowSums(Fish[,cols[i,1]:cols[i,2]]) #make count vector- first to last collumn of row i
@@ -52,16 +59,33 @@ for(i in 1:8) { #looking through first 2 rows, opening for loop
   n=n[n>0] #modify object to only keep numbers greater than 0
   print(n) # now no zeros
   g[i]=gsindex(n) # put first value in first cell, second in 2nd etc
+  fa[i]=fisher.alpha(n)
   r[i]= length(n) #tracking the raw number of detected species- what you foudn and not what was there
   f<-n/sum(n) #do before bootstrap to avoid messing with data
   h[i]=-sum(f*log(f)) #runs shannons
   n=sample(n,replace = T) #resampling with replacememt 
   bootstrapgsi[i]=gsindex(n) #runs boottsrao
 }
+fa
 g #suggests varuation bw sites and diversity 
 # in wet season- fish dissapear (last 4 number)
 #answering q of whether sp richnessestimates are replicable 
 plot(g[1:4],g[5:8])
+plot(fa,g) #not what is expected. fishers is getting confused by several points- should be linear trend. means either GS or Fishers is wrong.
+#gs is geomtric index- good way of estimating species richness
+#gs is assuming that sampling is about 1 individual per species (simplification- not true), eac species has own abundance (random set of abundances out in the world), assuming that distribution of abundances is evenley spaced, assumes exponential distribution (chau assumes unifrom dist which makes no sense)
+# if above is true then gs is very good richness estimate
+#Fishers assumes data follows log series (can be a disiater)
+# report both gsindex and fishers. 
+#refer to kerr and alroy
+
+#wilcoxon rank sum test to compare diversity index
+wilcox.test(fa[1:4], fa[5:8], paired=TRUE) #not signficant
+wilcox.test(g[1:4], g[5:8], paired=TRUE) 
+#perfect divide insifcates that there is a suggestive difference between the wet and dry season due to low sample size. want at least 20 site vs year combinations (400 samples)
+
+
+
 
 r 
 plot(r,g) #they somehwat agree- relatively linear
@@ -88,15 +112,19 @@ sum(is.na(Pesticides18))/length(unlist(Pesticides18))
 sum(is.na(Sediments18))/length(unlist(Sediments18))
 
 #removed first two collumns + new matrix of numbers
-Chem18a=Chem18[,-(1:2)]
+Chem18=as.data.frame(Chem18)
+Chem18a=matrix(as.numeric(unlist(Chem18[,-(1:2)])),35,21) #matrix of numners
+is.numeric(Chem18a) #make sure its numeric
+rownames(Chem18a)=Chem18[,2]
 Chem18a=matrix(as.numeric(unlist(Chem18a)),nrow(Chem18a),ncol(Chem18a))
+
 Nutrients18a=Nutrients18[,-(1:2)]
 Nutrients18a=matrix(as.numeric(unlist(Nutrients18a)),nrow(Nutrients18a),ncol(Nutrients18a))
 Pesticides18a=Pesticides18[,-(1:2)]
 Pesticides18a=matrix(as.numeric(unlist(Pesticides18a)),nrow(Pesticides18a),ncol(Pesticides18a))
 Sediments18a=Sediments18[,-(1:2)]
 Sediments18a=matrix(as.numeric(unlist(Sediments18a)),nrow(Sediments18a),ncol(Sediments18a))
-
+length(Chem18[,2])
 
 
 #replace NA's with median
@@ -131,28 +159,42 @@ cor(na.omit(Chem18a))
 #GREAT 5 FACTOR analysis
 #can do regression with 5 variables
 Chem18FA<-factanal(na.omit(Chem18a),factors=5,scores="regression")$scores
-plot(Chem18FA) #Do not want all data to be in one corner (clustered) with some outliers
+plot(Chem18FA,
+     main = "Factor Analysis Scores: 2018 Water Chemistry Data",
+     xlab = "Factor 1",
+     ylab = "Factor 2",
+     col = "red") #Do not want all data to be in one corner (clustered) with some outliers
+Chem18FA=rbind(Chem18FA[1:10,],NA,Chem18FA[11:33,])
+Chem18FA=rbind(Chem18FA,Chem18FA)
 summary(lm(log(Fish)~Chem18FA))
+Chem18FA
+Chem18
+
 
 #nutrients Factor analysis
 factanal(na.omit(Nutrients18a),factors=5)
 cor(na.omit(Nutrients18a))
 Nutr18FA<-factanal(na.omit(Nutrients18a),factors=5,scores="regression")$scores
-plot(Nutr18FA)
+plot(Nutr18FA, 
+     main = "Factor Analysis Scores: 2018 Nutrient Data",
+     xlab = "Factor 1",
+     ylab = "Factor 2",
+     col = "red")
 summary(lm(log(Fish)~Nutr18FA))
 
 
 #pesticides Factor analysis
 factanal(na.omit(Pesticides18a),factors=6)
+View(Pesticides18)
 cor(na.omit(Pesticides18a))
 Pest18FA<-factanal(na.omit(Pesticides18a),factors=5,scores="regression")$scores
 plot(Pest18FA)
 summary(lm(log(Fish)~Pest18FA))
 
 #sediments 
-factanal(na.omit(Sediments18a),factors=5)
+factanal(na.omit(Sediments18a),factors=3)
 cor(na.omit(Sediments18a))
-Sed18FA<-factanal(na.omit(Sediments18a),factors=5,scores="regression")$scores
+Sed18FA<-factanal(na.omit(Sediments18a),factors=3,scores="regression")$scores
 plot(Sed18FA)
 summary(lm(log(Fish)~Sed18FA))
 
@@ -160,6 +202,32 @@ summary(lm(log(Fish)~Sed18FA))
 
 
 
+SitesSeason=array(dim=68)
+SitesSeason[1:10]=1
+SitesSeason[11:20]=2
+SitesSeason[21:30]=3
+SitesSeason[31:34]=4
+SitesSeason[35:44]=5
+SitesSeason[45:54]=6
+SitesSeason[55:64]=7
+SitesSeason[65:68]=8
 
+#PCoa with bray curtis
+B<-cmdscale(vegdist(t(Fish)))
+Season=c(rep(1,34),rep(2,34)) #
+Season # usee to make characters different
+plot(B,cex=Season) #big circles=2020 data, small=2018
+plot(B,col=hsv(h=SitesSeason/9),pch=19) #sites are different colours. whopping huge difference
+colnames(Fish)
+summary(lm(B[,1]~as.factor(SitesSeason)))
+dim(B)        
+
+summary(lm(B[,1]~Chem18FA[,1]+ Nutr18FA[,1]+Sed18FA[,1]+as.factor(SitesSeason)))
+
+row.names(Chem18FA)
+
+#regression for each principle coordinate axis (Pcoa)
+summary(lm(B[,1]~Chem18FA+as.factor(SitesSeason)))
+summary(lm(B[,2]~Chem18FA+as.factor(SitesSeason)))
 
 
